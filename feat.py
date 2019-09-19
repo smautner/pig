48088394584cdbcd335739eff2bde9fc825ec344
S Mautner <mautner@cs.uni-freiburg.de>
2019-09-19 18:15:45 +0200

    first analysis done

--- feat.py
+++ feat.py
@@ -21,14 +21,14 @@ def cons_targets(pos,ali):
                 r.append(R/nu)
     return r 
 
-def conservation(cov, ali):
+def conservation(ali):
     # account for cov or not
-    covcnt =  cov.count("2")
-    nu,le = ali.shape
-    r = cons_targets(range(le),ali)
-    #print (cov)
-    #print([ 1 if b == '2' else a  for a,b in zip(r,cov) ])
-    return  np.mean(r), np.mean([ 1 if b == '2' else a  for a,b in zip(r,cov)  ])
+    covcnt =  ali.cov.count("2")
+    nu,le = ali.ali.shape
+    r = cons_targets(range(le),ali.ali)
+    #print (ali.cov)
+    #print([ 1 if b == '2' else a  for a,b in zip(r,ali.cov) ])
+    return  np.mean(r), np.mean([ 1 if b == '2' else a  for a,b in zip(r,ali.cov)  ])
         
 def percstem(stru,stacks):
     a = len(stru)
@@ -36,17 +36,20 @@ def percstem(stru,stacks):
     bigstacks = sum([ b-a+1 for a,b in stacks if b-a+1 > 2])
     return  allstacks/a,bigstacks/a 
 
-def get_sloppy(pos, ali, con):
+def get_sloppy(pos, ali):
     sloppy=0
     ok = 0
-    for p in pos: 
-        for a,b in zip( ali[:,p], ali[:,con.both[p]] ):
-            z = [a,b] if b > a else [b,a]
-            if z == ["G","U"]: # TODO  ok ich siollte ALLE strnagen paarings detectemn 
-                sloppy+=1
-            else:
-                ok +=1 
-    return  sloppy/(sloppy+ok) 
+    try:
+        for p in pos: 
+            for a,b in zip( ali.ali[:,p], ali.ali[:,ali.con.both[p]] ):
+                z = [a,b] if b > a else [b,a]
+                if z == ["G","U"]: # TODO  ok ich siollte ALLE strnagen paarings detectemn 
+                    sloppy+=1
+                else:
+                    ok +=1 
+        return  sloppy/(sloppy+ok) 
+    except: 
+        print ("getsloppy: p con.both ali.name, ali.blocks", p, ali.con.both, ali.name,ali.blocks )
 
 def weirdo_detection(ali):
     height, length = ali.shape
@@ -69,61 +72,72 @@ def weirdo_detection(ali):
             
 
 def checov(ali, pos, con, cov):
+    # chhecking covariance manually, this is necessary after we remove the worst hit line 
     covn = cov.count("2")*2
+    if covn == 0: return 0 
     loss = 0
     for p in pos:
         if cov[p]=='2':
             A=-1
             B=-1 
-            for a,b in zip(ali[:,p], ali[:,con.both[p]]):
-                if a != '.' and b!='.':
-                    if A==-1:
-                        A=a
-                        B=b
-                    if a!=A and b!=B: # when we find a single alternative, all is good
-                        break
-            else:
-                loss+=1  # all are the sam
+            try:
+                for a,b in zip(ali[:,p], ali[:,con.both[p]]):
+                    if a != '.' and b!='.':
+                        if A==-1:
+                            A=a
+                            B=b
+                        if a!=A and b!=B: # when we find a single alternative, all is good
+                            break
+                else:
+                    loss+=1  # all are the sam
+            except: 
+                print ("cov check: ali p con.both", ali, p, con.both )
                 
     return loss/covn
 
         
 
-def cov_sloppycov_disturbance_instem(stru,cov, ali, stacks,con):
+def cov_sloppycov_disturbance_instem(ali):
     # % covariance in stems in stems   # with or without shorts removed
     # sloppy
     # # optionally remove fail line 
 
     # get indices of stems (all and long)
-    hbonds = [ range(a,b+1) for a,b in stacks if b-a+1>2]
+    hbonds = [ range(a,b+1) for a,b in ali.blocks if b-a+1>2]
     hbonds = [a for b in hbonds for a in b]
-    hbonds2 = [ range(a,b+1) for a,b in stacks]
+    hbonds2 = [ range(a,b+1) for a,b in ali.blocks]
     hbonds2 = [a for b in hbonds2 for a in b]
 
-    pcov_large = sum([1 for a in hbonds if cov[a]=='2'])/len(hbonds)
-    pvoc_all = sum([1 for a in hbonds2 if cov[a]=='2'])/len(hbonds2)
+    pcov_large = sum([1 for a in hbonds if ali.cov[a]=='2'])/len(hbonds)
+    pvoc_all = sum([1 for a in hbonds2 if ali.cov[a]=='2'])/len(hbonds2)
     
     # now we get the sloppy covariance 
-    sl_large = get_sloppy(hbonds, ali, con)
-    sl_all = get_sloppy(hbonds2, ali, con)
+    sl_large = get_sloppy(hbonds, ali)
+    sl_all = get_sloppy(hbonds2, ali)
 
     # now we filter the most horrible line and do the stuff up there again. 
-    weird = weirdo_detection(ali)
-    ali2 = ali[[ a for a  in range(ali.shape[0]) if a not in weird]]
+    weird = weirdo_detection(ali.ali)
+    ali2 = ali.ali[[ a for a  in range(ali.ali.shape[0]) if a not in weird]]
     
-    return pcov_large,pvoc_all, sl_large, sl_all, checov(ali2,hbonds,con,cov), checov(ali2,hbonds2,con,cov)
+    return pcov_large,pvoc_all, sl_large, sl_all, checov(ali2,hbonds,ali.con,ali.cov), checov(ali2,hbonds2,ali.con,ali.cov)
 
-def stemlength(stacks):
-    s = [ b-a+1 for a,b in stacks  ]
+def stemlength(ali):
+    s = [ b-a+1 for a,b in ali.blocks  ]
     s.sort()
+    if len(s) ==2:
+        return 0,s[0],s[1]
+    elif len(s) ==1:
+        return 0,0,s[0]
+    elif len(s) == 0:
+        return 0,0,0
     return s[0],s[-2],s[-1]
 
 
-def stemconservation(stacks,ali):
-    stacks2 = [ (a,b) for a,b in stacks if b-a+1 > 2  ]
-    return cons_stem(stacks,ali), cons_stem(stacks2,ali)
+def stemconservation(ali):
+    stacks2 = [ (a,b) for a,b in ali.blocks if b-a+1 > 2  ]
+    return cons_stem(ali.blocks,ali.ali), cons_stem(stacks2,ali.ali)
 
-def cons_stem(stacks, ali):
+def cons_stem(stacks,ali):
 
     targets = set()
     for a,b in stacks:
@@ -135,3 +149,7 @@ def cons_stem(stacks, ali):
                 targets.add(z)
 
     return np.mean(cons_targets(targets,ali))
+
+
+def beststem(ali):
+    pass
--- pig.py
+++ pig.py
@@ -1,90 +1,10 @@
-import feat 
-import numpy as np
-def start(st,text):
-    return st[:len(text)]==text
-
-def readfile(fname="asd.sto"):
-    asd = open("asd.sto").readlines()
-
-    alignment=[]
-    for line in asd:
-        if start(line,"#=GC SS_cons"):
-            stru = line.split()[-1]
-        elif start(line,"#=GC cov_SS_cons"):
-            cov = line.split()[-1]
-        elif line.startswith("#"):
-            # ignore all other lines
-            continue
-        elif line.startswith("/"):
-            # end of file somehow there is a marker for that 
-            continue
-        else:
-            alignment.append(line.split()[-1])
-    alignment = np.array([list(a) for a in alignment])
-    return fname, alignment, stru, cov 
-
-class bidir:
-    def __init__(self):
-        self.f ={}
-        self.b ={}
-    def lol(self,a,b):
-        self.f[a]=b
-        self.b[b]=a
-    def fin(self):
-        self.both = dict(self.f)
-        self.both.update(self.b)
-
-def getblocks(stru):
-    # shoud return a list of (start,stop)
-    
-    # get start/end 
-    stack=[]
-    bdir = bidir()
-    for i,l in enumerate(stru):
-        if l == '<':
-            stack.append(i)
-        if l == '>':
-            bdir.lol(stack.pop(),i)
-    bdir.fin()
-    unpaired= '._:-'
-    mode = 'def'
-    blocks = []
-    for i,l in enumerate(stru):
-        if mode == 'def':
-            if l == '<' or l == '>':
-                start = i 
-                mode = l
-
-        elif mode == '<':
-            if l != '<':
-                blocks.append((start,i-1))
-                if l in unpaired:
-                    mode = 'def'
-                if l == '>':
-                    start = i 
-                    mode  = l
-
-        elif mode == '>':
-            if l != '>':
-                blocks.append((start,i-1))
-                if l in unpaired:
-                    mode = 'def'
-                if l == '<':
-                    start = i 
-                    mode  = l
-    
-    realblocks = [(a-5,bdir.f[e]+5) if stru[a]=='<' else (bdir.b[a]-5,e+5)  for a,e in blocks]
-    return realblocks, blocks, bdir
-        
-
-def makevec(ali, stru, cov):
-    _, blocks,con = getblocks(stru)
-    return [a  for b in [feat.conservation(cov,ali),
-                        feat.cov_sloppycov_disturbance_instem(stru,cov,ali,blocks, con),
-                        feat.stemconservation(blocks, ali), 
-                        feat.stemlength(blocks)] for a in b  ]
-    
 
 
-name, ali, stru, cov = readfile()
-print(makevec(ali, stru, cov))
+from sklearn.model_selection import train_test_split
+from loadfiles import loaddata
+
+
+X,y = loaddata("/home/pig/data")
+X1,X2,Y1,Y2 = train_test_split(test_size=.33, random_state=1337)
+
+
