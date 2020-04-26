b20af195328a88ccbcbc832a0184a1e9ee2d892a
MatthiasHerrmann <herrmannmatthias@gmx.net>
2020-04-26 23:49:36 +0200

    Improved Yaoscores
    - Moved Yaoscores folder into the data folder
    - logistic_regression.py: Removed execute_cmfinder04()
    - cmfinder.py: Added execute_cmfinder04() and modified it so it now outputs .json files with filenames as key and their yaoscores as values
    
    loaddata():
    - changed the seed to be random if no seed is given or random otherwise (If no given seed => default is None seed(None) = seed is system time
    - removed getall from the input
    - changed print statement to "loadfiles.py"
    
    fnames_to_dict():
    - removed getall from the input

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,9 +1,9 @@
 from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
 from numpy import reshape, concatenate
 from numpy import chararray
-import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
 #from loadfiles import loaddata
 
 
@@ -26,33 +26,6 @@ from sklearn.model_selection import cross_val_score
 #         n = json.load(f)
 #     return p, n
 
-def execute_cmfinder04(path="cmfinder-0.4.1.18/bin/cmfinder04",
-                       data="data", output="", overwrite=False):
-    """Executes the cmfinder program to calculate the yao-score
-       with all samples in the given data folder.
-    Args:
-      path(String): The path for the cmfinder04 tool.
-      data(String): The path of the datafolder.
-      output(String): Path for the output files.
-      overwrite(Bool): Set True if existing output files should be overwritten.
-    """
-    if output == "":
-        output = "yaoscores"
-        os.system("mkdir -p " + output)
-    else:
-        output = output + "/yaoscores"
-        os.system("mkdir -p " + output)
-    for directory in os.listdir(data):
-        if overwrite == True:
-            os.remove(output + "/" + directory)
-        for filename in os.listdir(data + "/" + directory):
-            os.system(path + " --summarize --summarize-gsc \
-                      --summarize-no-de-tag --fragmentary " + data + "/" +
-                      directory + "/" + filename +
-                      " | grep -oP '(?<=yaoFormulaScore=)[0-9]+.[0-9]+' >> " +
-                      output + "/" + directory)
-
-
 def log_reg(pos, neg):
     """Builds a Logistic Regression model using the Yao-score
        of the given sample data as the feature and returns it.
@@ -61,7 +34,7 @@ def log_reg(pos, neg):
       neg (Array[String]): An array of paths to proven negative yao-scores.
 
     Returns:
-      lr (LogisticRegression): The trained model.
+      cv (ndarray[float64]): The Cross validation score
     """
     X = []
     for i in neg:
@@ -73,13 +46,11 @@ def log_reg(pos, neg):
             X = concatenate((X, reshape(posfile.read().split(), (-1, 1))))
     posf = len(X) - negf
     y = [0]*negf + [1]*posf
-    lr = LogisticRegression().fit(X, y)
+    lr = LogisticRegression()
     # X_scaled = StandardScaler().fit_transform(X)
-    cv = cross_val_score(lr, chararray.astype(X, float), y)
-    return lr, cv 
+    cv = cross_val_score(lr, chararray.astype(X, float), y, scoring="f1")
+    return cv 
 
 
 if __name__ == "__main__":
-    # execute_cmfinder04() #-- Takes a LONG time
-    lr, cv = log_reg(["yaoscores/pos", "yaoscores/pos2"], ["yaoscores/neg"])
-    # test_ali("data/pos/8-1281-0-1.sto", lr)
+    cv = log_reg(["yaoscores/pos", "yaoscores/pos2"], ["yaoscores/neg"])
