46e1345d978fa9555299c7a3293418c0690072c6
MatthiasHerrmann <herrmannmatthias@gmx.net>
2020-05-29 23:38:36 +0200

    rps should now do what its supposed to
    Changenotes:
    - Removed random_parameter_search_performance.py since it got replaced by rps.py
    - feature_selection() should now also return func_names
    - Instead of using the scorer function score will now return best_score_ from RSCV and the best estimator
    - replaced try/except in load_data() in the help functions with a proper check if a file with that name exists.
    - Commented out the featurelists.append() of just all the featurelists since it seems to be not required (possible removal?)
    - rps is now aware of splits and returns more variables that are useful for creating the results
    - doajob() now dumps its result in a .json file with its taskid as a name
    - getresults() analyzes the results of doajob() and dumps them in "results.json"

--- feature_selection.py
+++ feature_selection.py
@@ -7,7 +7,6 @@ from sklearn.svm import SVC
 from IPython.display import display, HTML
 from multiprocessing import Pool
 from functools import partial
-from other.help_functions import load_data, pd_dataframe, kfold
 
 
 #####################
@@ -46,7 +45,7 @@ def select_k_best(X_data, y_data, df, score_func=chi2, k=20):
 
 
 def rfecv(X_data, y_data, df, estimator, step=1, cv=3):
-    clf = RFECV(estimator, step, cv)
+    clf = RFECV(estimator, step=step, cv=cv)
     clf.fit(X_data, y_data)
     return [b for a, b in zip(clf.get_support(), df.columns) if a != False]
 
@@ -60,7 +59,7 @@ def smap(f):
     return f()
 
 
-def feature_selection(X_train, y_train, df, processes=None, debug=False):
+def feature_selection(X_train, y_train, df, processes=8, debug=False):
     """Executes the feature selection process with multiprocessing.
     Args:
       X_train (ndarray)
@@ -72,42 +71,57 @@ def feature_selection(X_train, y_train, df, processes=None, debug=False):
     Returns:
       featurelists(List)
       """
+    func_names = []
+
     reli = ReliefF()
     reli.fit(X_train, y_train)
     rfecv_estimator = SVC(kernel="linear")
+
+    functions = []
+    func_names = []
     if debug:
         functions = [
             partial(lasso, X_train, y_train, df, alpha=.05),
             partial(relief, X_train, y_train, reli, df, 40),
             partial(variance_threshold, X_train, y_train, df, threshold=1)]
+        func_names.extend(["Lasso: 0.05", "Relief: 40", "VarThresh: 1"])
     else:
-        functions = []
         for alpha in [.05, 0.1]:  # Lasso
             functions.append(partial(lasso, X_train, y_train, df, alpha=alpha))
+            func_names.append(f"Lasso: {alpha}")
         for features in [40, 60, 80]:  # Relief
             functions.append(partial(relief, X_train, y_train, reli, df, features))
+            func_names.append(f"Relief: {features}")
         for threshold in [1]:  # Variance Threshold
             functions.append(partial(variance_threshold, X_train, y_train, df, threshold=1))
+            func_names.append(f"VarThresh: {alpha}")
         for k in [20]:  # Select K Best
             functions.append(partial(select_k_best, X_train, y_train, df, k=k))
+            func_names.append(f"SelectKBest: {k}")
         for stepsize in [1, 2, 3]:  # RFECV (Testing)
             functions.append(partial(rfecv, X_train, y_train, df, rfecv_estimator, step=stepsize))
+            func_names.append(f"RFECV: {stepsize}")
     with Pool(processes) as pool:
         featurelists = pool.map(smap, functions)
-    featurelists.append(list(df.columns)) # Maybe should be pulled out of the main function
+    # featurelists.append(list(df.columns)) # Maybe should be pulled out of the main function
+    # I commented this one out for now since it seems like its not needed at all. Possible removal.
     tmp = pd.DataFrame([[1 if f in featurelist else 0 for f in df.columns]
                         for featurelist in featurelists], columns=df.columns)
     display(HTML(tmp.loc[:, (tmp != 0).any(axis=0)].to_html()))
-    return featurelists
+    return featurelists, func_names
 
 
 if __name__ == "__main__":
+    import other.help_functions as h
     randseed = 42
     debug = True
 
-    p, n = load_data(debug)
-    X, Y, df = pd_dataframe(p, n)
-    folds = kfold(X, Y, n_splits=2, randseed=randseed)
+    p, n = h.load_data(debug)
+    X, Y, df = h.pd_dataframe(p, n)
+    folds = h.kfold(X, Y, n_splits=2, randseed=randseed)
     featurelists = []
+    func_names = []
     for X_train, X_test, y_train, y_test in folds:
-        featurelists += feature_selection(X_train, y_train, df, debug=debug)
+        fs = feature_selection(X_train, y_train, df, debug=debug)
+        featurelists.append(fs[0])
+        func_names.append(fs[1])
--- pig.py
+++ pig.py
@@ -1,66 +1,80 @@
 import sys
 import numpy as np
 import other.randomsearch as  rs
-#import basics as b#####
+import os
 import other.help_functions as h
-from feature_selection import feature_selection as fs
+from feature_selection import feature_selection
 import rps
 
 ############
 #
 #############
 
-def getfeaturelist(n_splits=2, randseed=42, debug=True):
-    
+
+def getfeaturelist(n_splits, randseed, debug):
     p, n = h.load_data(debug)
     X, Y, df = h.pd_dataframe(p, n)
     folds = h.kfold(X, Y, n_splits=n_splits, randseed=randseed)
     featurelists = []
+    func_names = []
     for X_train, X_test, y_train, y_test in folds:
-        featurelists += fs(X_train, y_train, df, debug=debug)
-    h.dumpfile(featurelists, "ftlist.json")
-    rps.maketasks(featurelists, p, n, randseed, n_splits)
-    return featurelists
-    
+        fs = feature_selection(X_train, y_train, df, debug=debug)
+        featurelists.append(fs[0])
+        func_names.append(fs[1])
+    #h.dumpfile(featurelists, "tmp/ftlist.json") #Not really required but maybe would help?
+    #h.dumpfile(func_names, "tmp/funcnames.json") #Same as above.
+    rps.maketasks(featurelists, func_names, p, n, randseed, n_splits) # Creates "tmp/tasks" file
 
-def gettasks():
-    ftlist  = h.loadfile("ftlist.json")
-    tasks = [(clf,param,ftli)for clf,param in zip(rs.classifiers,rs.param_lists) for ftli in ftlist]
-    return tasks
+def getnumtasks():
+    tasks  = np.load("tmp/tasks", allow_pickle=True)
+    return len(tasks)
 
 def gettasks_annotated():
-    ftlist  = h.loadfile("ftlist.json")
+    ftlist  = h.loadfile("tmp/ftlist.json")
     tasks = [(clf,param,ftli, clfname,ftid)for clf,param,clfname in zip(rs.classifiers,rs.param_lists, rs.clfnames) for ftid,ftli in enumerate(ftlist)] 
     return tasks
 
 def doajob(idd, debug):
-    tasks = np.load("tasks", allow_pickle=True)
-    res, best_esti = rps.random_param_search(tasks[idd], n_jobs=4, debug=debug)
-    print(res, best_esti)
-        
+    tasks = np.load("tmp/tasks", allow_pickle=True) ###
+    foldnr, best_score, best_esti, ftlist, fname = rps.random_param_search(tasks[idd], n_jobs=4, debug=debug)
+    #np.array([best_score, best_esti, ftlist, fname]).dump(f"tmp/results/{idd}")
+    h.dumpfile([foldnr, best_score, str(best_esti), ftlist, fname], f"tmp/results/{idd}.json")
+    #print(best_score, best_esti, ftlist, fname)
 
-def readresult(fname):
-    a= open(fname,"r").read()
-    dic,numerics = a.split("}")
-    params = eval(dic+"}")
-    f1,perf,jid,time = [float(a) for a in numerics.split()]
-    return f1,perf, dic , jid , time
-    
+def getresults():
+    results = {}
+    for rfile in os.listdir("tmp/results"):
+        f = h.loadfile(f"tmp/results/{rfile}")
+        if f[0] in results:
+            if f[1] > results[f[0]][1]:
+                results[f[0]] = f
+        else:
+            results[f[0]] = f
+    h.dumpfile(results, "results.json")
+    return results
 
 
 if __name__ == "__main__":
     debug = True
-    if sys.argv[1] == 'maxtask':
-        print(len( gettasks_annotated() )) # for use in qsub.. 
+    n_splits = 2
+    randseed = 42
 
-    elif sys.argv[1] == 'showft':
-        features = h.loadfile("ftlist.json")
-        ft= list(features[int(sys.argv[2])])
-        import pprint
-        pprint.pprint(ft)
-        print("numft:",len(ft))
+    if not os.path.exists("tmp/results"):
+        print("Creating tmp/results directory")
+        os.makedirs("tmp/results")
+    
+    if sys.argv[1] == 'maxtask':
+        print(getnumtasks()) # for use in qsub.. 
+
+    elif sys.argv[1] == 'getresults':
+        getresults()
+
+    elif sys.argv[1] == 'showresult':
+        print(h.loadfile("results.json")[sys.argv[2]])
+
     elif sys.argv[1] == 'makeftlist':
-        getfeaturelist()
+        getfeaturelist(n_splits, randseed, debug)
+
     else:
         idd = int(sys.argv[1])-1
         doajob(idd, debug)
