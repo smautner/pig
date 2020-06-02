71143a20532e212413be8b3b410678b594b27c9e
MatthiasHerrmann <herrmannmatthias@gmx.net>
2020-06-02 15:10:33 +0200

    Reworked Feature Selection and pig
    Changelog:
    ------------
    - Completely redone featurelist calculation so it also uses the Cluster now
    - Changed some data structures (featurelists are now dictionaries with their foldnumber as keys)
    - Changed fold numbers to 5 inner and 10 outer folds and disabled debug on default
    - Added function to calculate "k top features" using the featurelists
    - Added Cleanup function
    - Made best_esti more readable in its dumped file

--- pig.py
+++ pig.py
@@ -3,80 +3,152 @@ import numpy as np
 import other.randomsearch as  rs
 import os
 import other.help_functions as h
-from feature_selection import feature_selection
-import rps
+import other.feature_selection as fs
+import other.rps
+from sklearn.preprocessing import StandardScaler
 
-############
-#
+
+
+def cleanup(pn=False):
+    """Cleans up the tmp folder to prevent inconsistencies when toggling debug.
+    """
+    import shutil
+    if os.path.exists("tmp/fs_results"):
+        shutil.rmtree("tmp/fs_results")
+    if os.path.exists("tmp/rps_results"):
+        shutil.rmtree("tmp/rps_results")
+    for file in os.listdir("tmp"):
+        if not (file == "pn.json" or file == "pnd.json") or pn:
+            os.remove(f"tmp/{file}")
+            print(f"Removed tmp/{file}")
+        
+
+#############
+# Make Featurelists
 #############
 
 
-def getfeaturelist(n_splits, randseed, debug):
+def makefltasks(n_splits, randseed, debug):
+    """Creates tasks the cluster uses to create featurelists."""
     p, n = h.load_data(debug)
     X, Y, df = h.pd_dataframe(p, n)
+    X = StandardScaler().fit_transform(X)
     folds = h.kfold(X, Y, n_splits=n_splits, randseed=randseed)
-    featurelists = []
-    func_names = []
-    for X_train, X_test, y_train, y_test in folds:
-        fs = feature_selection(X_train, y_train, df, debug=debug)
-        featurelists.append(fs[0])
-        func_names.append(fs[1])
-    #h.dumpfile(featurelists, "tmp/ftlist.json") #Not really required but maybe would help?
-    #h.dumpfile(func_names, "tmp/funcnames.json") #Same as above.
-    rps.maketasks(featurelists, func_names, p, n, randseed, n_splits) # Creates "tmp/tasks" file
+    tasks = fs.maketasks(folds, df, debug)
+    print(f"Created {len(tasks)} FS tasks.")
 
-def getnumtasks():
-    tasks  = np.load("tmp/tasks", allow_pickle=True)
-    return len(tasks)
+def calculate_featurelists(idd):
+    """Executed by Cluster"""
+    foldnr, fl, fname = fs.feature_selection(idd)
+    h.dumpfile([foldnr, fl, fname], f"tmp/fs_results/{idd}.json")
 
-def gettasks_annotated():
-    ftlist  = h.loadfile("tmp/ftlist.json")
-    tasks = [(clf,param,ftli, clfname,ftid)for clf,param,clfname in zip(rs.classifiers,rs.param_lists, rs.clfnames) for ftid,ftli in enumerate(ftlist)] 
-    return tasks
+def gather_featurelists(debug):
+    """Collect results to create the proper featurelists.
+    Also creates tmp/rps_tasks for RPS.
+    Note: The debug variable needs to be the same value as makefltasks uses."""
+    featurelists = {}
+    for ftfile in os.listdir("tmp/fs_results"):
+        foldnr, fl, fname = h.loadfile(f"tmp/fs_results/{ftfile}")
+        if foldnr in featurelists:
+            featurelists[foldnr].append((fl, fname))
+        else:
+            featurelists[foldnr] = [(fl, fname)]
+    h.dumpfile(featurelists, "tmp/ftlists.json") # Used to rank features
+    p, n = h.load_data(debug)
+    tasks = rps.maketasks(featurelists, p, n, randseed, n_splits) # Creates "tmp/rps_tasks" file
+    print(f"Created {len(tasks)} RPS tasks.")
+
+def get_top_features(k=None):
+    """Optional function. Returns the k most used features.
+    Not very efficient but probably does the job for now."""
+    from collections import Counter
+    featurelists = h.loadfile("tmp/ftlists.json")
+    fl = list(featurelists.values())
+    c = Counter()
+    for fold in fl:
+        for ftlist in fold:
+            c.update(ftlist[0])
+    return c.most_common(k)
+        
+
+
+#############
+# Random Parameter Search
+#############
+
 
 def doajob(idd, debug):
-    tasks = np.load("tmp/tasks", allow_pickle=True) ###
-    foldnr, best_score, best_esti, ftlist, fname = rps.random_param_search(tasks[idd], n_jobs=4, debug=debug)
-    #np.array([best_score, best_esti, ftlist, fname]).dump(f"tmp/results/{idd}")
-    h.dumpfile([foldnr, best_score, str(best_esti), ftlist, fname], f"tmp/results/{idd}.json")
-    #print(best_score, best_esti, ftlist, fname)
+    tasks = np.load("tmp/rps_tasks", allow_pickle=True)
+    foldnr, best_score, best_esti, ftlist, fname = rps.random_param_search(tasks[idd], n_jobs=24, debug=debug)
+    best_esti = (type(best_esti).__name__, best_esti.get_params()) # Creates readable tuple that can be dumped.
+    h.dumpfile([foldnr, best_score, best_esti, ftlist, fname], f"tmp/rps_results/{idd}.json")
+    return best_esti
 
 def getresults():
     results = {}
-    for rfile in os.listdir("tmp/results"):
-        f = h.loadfile(f"tmp/results/{rfile}")
+    for rfile in os.listdir("tmp/rps_results"):
+        f = h.loadfile(f"tmp/rps_results/{rfile}")
         if f[0] in results:
-            if f[1] > results[f[0]][1]:
-                results[f[0]] = f
+            if f[1] > results[f[0]][0]:
+                results[f[0]] = f[1:]
         else:
-            results[f[0]] = f
+            results[f[0]] = f[1:]
     h.dumpfile(results, "results.json")
     return results
 
 
 if __name__ == "__main__":
-    debug = True
-    n_splits = 2
+    debug = False
+    n_splits = 10
     randseed = 42
 
-    if not os.path.exists("tmp/results"):
-        print("Creating tmp/results directory")
-        os.makedirs("tmp/results")
-    
-    if sys.argv[1] == 'maxtask':
-        print(getnumtasks()) # for use in qsub.. 
+    if not os.path.exists("tmp"):
+        print("Creating tmp directory")
+        os.makedirs("tmp")
+    if not os.path.exists("tmp/fs_results"):
+        print("Creating tmp/fs_results directory")
+        os.makedirs("tmp/fs_results")
+    if not os.path.exists("tmp/rps_results"):
+        print("Creating tmp/rps_results directory")
+        os.makedirs("tmp/rps_results") 
+
+
+    if sys.argv[1] == 'makefltasks':
+        makefltasks(n_splits, randseed, debug)
+
+    elif sys.argv[1] == 'calcfl':
+        idd = int(sys.argv[2])-1
+        calculate_featurelists(idd)
+
+    elif sys.argv[1] == 'gatherfl':
+        gather_featurelists(debug)
+
+    elif sys.argv[1] == 'calcrps':
+        idd = int(sys.argv[2])-1
+        doajob(idd, debug)
 
     elif sys.argv[1] == 'getresults':
         getresults()
 
     elif sys.argv[1] == 'showresult':
-        print(h.loadfile("results.json")[sys.argv[2]])
+        if sys.argv[2] == 'all':
+            print(h.loadfile("results.json"))
+        else:
+            print(h.loadfile("results.json")[sys.argv[2]])
 
-    elif sys.argv[1] == 'makeftlist':
-        getfeaturelist(n_splits, randseed, debug)
-
+    elif sys.argv[1] == 'topfeatures':
+        if len(sys.argv) == 1:
+            get_top_features()
+        else:
+            get_top_features(sys.argv[2])
+    elif sys.argv[1] == cleanup:
+        if len(sys.argv) == 1:
+            get_top_features()
+        elif sys.argv[2] == 'True':
+            get_top_features(True)
+        else:
+            print("Usage: cleanup (True if pn and pnd should also be removed)")
     else:
-        idd = int(sys.argv[1])-1
-        doajob(idd, debug)
+        print("Usage: makefltasks -> calcfl(Cluster) -> gatherfl -> calcrps(Cluster) -> getresults -> showresults")
 
 
--- runall_fs_sge.sh
+++ runall_fs_sge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -cwd
 #$ -l h_vmem=512M
-#$ -pe smp 24
+#$ -pe smp 8
 #$ -R y
 # -o and -e need to different for each user.
 #$ -o /home/guest10/BioProject/JOBZ/pig_o/$JOB_ID.o_$TASK_ID
 #$ -e /home/guest10/BioProject/JOBZ/pig_e/$JOB_ID.e_$TASK_ID
 
-python pig.py calcrps $SGE_TASK_ID
+python pig.py calcfl $SGE_TASK_ID
 #qsub -V -t 1-35  runall_sge.sh
