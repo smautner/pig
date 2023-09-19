a4ee3ccfd7b946a8cf6f9deaf8111029b3b7ca6d
S Mautner <mautner@cs.uni-freiburg.de>
2023-09-19 16:57:01 +0000

    sincos positional encoding

--- yoda/ml/nnembedder.py
+++ yoda/ml/nnembedder.py
@@ -3,6 +3,34 @@ import ubergauss.tools as ut
 
 import networkx.linalg as nxlin
 import numpy as np
+
+
+
+
+
+import torch,math
+def make_sincos_index(max_len, dim=4):
+    # indices = np.repeat ( Range(maxlen), maxlen).reshape((maxlen,maxlen))
+    pe = torch.zeros((max_len, dim))
+    # print(pe)
+    position = torch.arange(0, max_len).unsqueeze(1).type(torch.FloatTensor)
+    div_term = torch.exp( torch.arange(0, dim, 2).type(torch.FloatTensor) * -(math.log(10000.0) / dim))
+
+    # print(torch.sin(position * div_term))
+    pe[:,0::2] = torch.sin(position * div_term)
+    pe[:,1::2] = torch.cos(position * div_term)
+
+    pattern =  pe.numpy()
+    return  np.repeat(pattern, max_len).reshape((4,max_len,max_len))
+
+
+def test_make_sincos_index():
+    layers = make_sincos_index(20)
+    import structout as so
+    print (layers.shape)
+    for layer in layers:
+        so.heatmap(layer)
+
 def makematrix(graph, maxlen = None):
 
     startpadding = 5
@@ -23,17 +51,19 @@ def makematrix(graph, maxlen = None):
     sorted_nodes = sorted(graph.nodes)
     adjacency_matrix = nxlin.adjacency_matrix(graph,nodelist=sorted_nodes).todense()
     # now we need to add another axis and add node infos
-    res = np.zeros((11, maxlen, maxlen))
-    indices = np.repeat ( Range(maxlen), maxlen).reshape((maxlen,maxlen))
+    res = np.zeros((17, maxlen, maxlen),dtype = np.float32)
+    indices = make_sincos_index(maxlen)
+
     def makeonehot(char):
         return np.repeat ( [ 1 if graph.nodes[n]['label'] == char else 0 for n in sorted_nodes ] , maxlen).reshape((maxlen,maxlen))
 
     ONEHOT = np.array( Map(makeonehot, 'AUGC'))
     res[:4] = ONEHOT
-    res[4] = indices
-    res[5] = adjacency_matrix
-    res[6] = indices.T
-    res[7:] = np.transpose(ONEHOT, (0,2,1))
+    res[4:8] = np.transpose(ONEHOT, (0,2,1))
+
+    res[8:12] = indices
+    res[12:16] = np.transpose(indices, (0,2,1))
+    res[16] = adjacency_matrix
 
     return res
 
@@ -71,10 +101,19 @@ if __name__ == f"__main__":
 
 
 
+import ubergauss.optimization as uo
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-class CustomDataset(Dataset):
+
+
+
+
+
+##################
+# old loader that builds all matrices instantly
+####################
+class CustomDataset_old(Dataset):
     def __init__(self, numpy_array_list, labels):
         self.data = numpy_array_list
         self.labels = labels
@@ -87,20 +126,58 @@ class CustomDataset(Dataset):
         label = self.labels[idx]
         return torch.from_numpy(sample), torch.tensor(label)
 
-
-def makeloader(arrays_labels, batch_size):
+def makeloader_old(arrays_labels, batch_size):
     return   DataLoader(CustomDataset(*arrays_labels), batch_size=batch_size, shuffle=True)
 
+def torchloader_old(batch_size, alignments, labels):
+    X  = np.array(toarray(alignments))
 
-import ubergauss.optimization as uo
-def torchloader(batch_size, alignments, labels):
-    X  = np.array(toarray(alignments)) # HOPE THIS WORKS
-    train, test = next(uo.groupedCV(n_splits = 3).split(X,labels,labels))
+    train, test = next(uo.groupedCV(n_splits = 3).split(_,labels,labels))
     labels = np.array(labels)
     tr = X[train], labels[train]
     te = X[test], labels[test]
-
     return makeloader(tr, batch_size), CustomDataset(*tr), CustomDataset(*te)
+
+
+
+
+
+
+
+
+
+
+##################
+# improved loader that builds matrixces on the fly
+####################
+class CustomDataset(Dataset):
+    def __init__(self, alignments, labels, maxlen):
+        self.maxlen= maxlen
+        self.data = alignments
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = makematrix(self.data[idx], self.maxlen)
+        label = self.labels[idx]
+        return torch.from_numpy(sample), torch.tensor(label)
+
+def makeloader(ali,label, batch_size, maxlen):
+    return   DataLoader(CustomDataset(ali,label, maxlen), batch_size=batch_size, shuffle=True)
+
+def torchloader(batch_size, alignments, labels):
+
+    graphs = [a.graph for a in alignments]
+    maxlen = max(map(len, graphs))
+
+    train, test = next(uo.groupedCV(n_splits = 3).split(None,labels,labels))
+    labels = np.array(labels)
+    tr = labels[train]
+    te = labels[test]
+    train_alignments  = [graphs[i] for i in train]
+    return makeloader(train_alignments,tr, batch_size, maxlen), CustomDataset(train_alignments, tr, maxlen), CustomDataset([graphs[i] for i in test], te, maxlen)
 
 
 
